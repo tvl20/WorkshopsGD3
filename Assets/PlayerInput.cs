1e1451f2de3205b7a556996c496a6299e22beea1
Unknown <teunvanlaarhoven@gmail.com>
2018-09-06 11:20:59 +0200

    Added swipe input

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float deadzone = 50;
+    public float doubleTapTimeframe = 0.5f;
+
+    private Vector2 tapOrigin = Vector2.zero;
+    private bool doubleTap = false;
+    private float doubleTapCounter = 0;
+
+    private void Update()
+    {
+        if (doubleTap)
+        {
+            if (doubleTapCounter < doubleTapTimeframe)
+            {
+                doubleTapCounter += Time.deltaTime;
+            }
+            else
+            {
+                doubleTapCounter = 0;
+                doubleTap = false;
+            }
+        }
+
+        TapInputGesture gesture = GetInputGesture();
+        if (gesture != TapInputGesture.None)
+        {
+            Debug.Log(gesture);
+        }
+    }
+
+    // todo: pretty this up
+    public TapInputGesture GetInputGesture()
+    {
+        if (Input.touchCount == 0)
+        {
+            return TapInputGesture.None;
+        }
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            tapOrigin = touch.position;
+        }
+        else if (touch.phase == TouchPhase.Ended)
+        {
+            return tapDirection(tapOrigin, touch.position, deadzone);
+        }
+
+        return TapInputGesture.None;
+    }
+
+    private TapInputGesture tapDirection(Vector2 origin, Vector2 destination, float deadzone)
+    {
+        float deltaX = destination.x - origin.x;
+        float deltaY = destination.y - origin.y;
+
+        if (Math.Abs(deltaX) < deadzone && Math.Abs(deltaY) < deadzone)
+        {
+            // The taps were within the deadzone, thus it is a tap or double tap
+            if (doubleTap)
+            {
+                if (doubleTapCounter < doubleTapTimeframe)
+                {
+                    doubleTapCounter = 0;
+                    doubleTap = false;
+                    return TapInputGesture.DoubleTap;
+                }
+            }
+
+            doubleTap = true;
+            return TapInputGesture.Tap;
+        }
+
+        if (Math.Abs(deltaX) > Math.Abs(deltaY))
+        {
+            // we are moving horizontally
+            if (deltaX < 0)
+            {
+                return TapInputGesture.SwipeLeft;
+            }
+            else if (deltaX > 0)
+            {
+                return TapInputGesture.SwipeRight;
+            }
+        }
+        else
+        {
+            // we are moving vertically
+            if (deltaY < 0)
+            {
+                return TapInputGesture.SwipeDown;
+            }
+            else if (deltaY > 0)
+            {
+                return TapInputGesture.SwipeUp;
+            }
+        }
+
+        // Code shouldnt reach this, but this is needed for the compiler
+        return TapInputGesture.Tap;
+    }
+}
---
+++ Assets/TapInputGesture.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TapInputGesture
+{
+    Tap,
+    DoubleTap,
+    SwipeUp,
+    SwipeDown,
+    SwipeLeft,
+    SwipeRight,
+    None
+}
