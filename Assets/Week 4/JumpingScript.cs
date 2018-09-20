ecc494e3c3b96a2444ba4daad9b1c5c4e4f06383
Unknown <teunvanlaarhoven@gmail.com>
2018-09-20 20:58:38 +0200

    addes simple jump animation for a cube

---
+++ Assets/Week 4/JumpingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingScript : MonoBehaviour
+{
+	public Animator cubeAnimator;
+	public string parameterName;
+
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			cubeAnimator.SetBool(parameterName, true);
+		}
+		else if (Input.GetKeyUp(KeyCode.Space))
+		{
+			cubeAnimator.SetBool(parameterName, false);
+		}
+	}
+}
